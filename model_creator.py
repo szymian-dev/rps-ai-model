7c7fa04d7414bff2a49820404a6566e086434212
adrk17 <adrian.krupa17@gmail.com>
2024-10-11 11:01:19 +0200

    #4 Fix

--- model_creator.py
+++ model_creator.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 from tensorflow.keras import layers, models
+import keras
 
 def create_simple_cnn_model(input_shape=(224, 224, 3), num_classes=3):
     model = models.Sequential()
@@ -10,4 +11,31 @@ def create_simple_cnn_model(input_shape=(224, 224, 3), num_classes=3):
     model.add(layers.Flatten())
     model.add(layers.Dense(64, activation='relu'))
     model.add(layers.Dense(num_classes, activation='softmax'))  # Ostatnia warstwa z softmax
+    return model
+
+def create_cnn_model_reguliarized(input_shape=(224, 224, 3), num_classes=3):
+    model = models.Sequential()
+    model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=input_shape))
+    model.add(layers.BatchNormalization())
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Dropout(0.2))
+    model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Dropout(0.2))
+    model.add(layers.Conv2D(256, (3, 3), activation='relu'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Flatten())
+    model.add(layers.Dropout(0.4))
+    model.add(layers.Dense(256, activation='relu', kernel_regularizer=keras.regularizers.l2(0.001)))
+    model.add(layers.BatchNormalization())
+    model.add(layers.Dropout(0.2))
+    model.add(layers.Dense(num_classes, activation='softmax'))
     return model
