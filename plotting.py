c54926711a81f2b2fccc655096e5829118339750
adrk17 <adrian.krupa17@gmail.com>
2024-11-13 16:09:44 +0100

    Model evaluations and new model trained

--- plotting.py
+++ plotting.py
@@ -1,5 +1,8 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
     
 def plot_accuracy(history):
     acc = history['acc']
@@ -67,48 +70,35 @@ def plot_accuracy_and_loss(history):
 def plot_history_csv(csv_file):
     history = pd.read_csv(csv_file)
     
-    required_columns = ['accuracy', 'val_accuracy', 'loss', 'val_loss']
+    required_columns = ['acc', 'val_acc', 'loss', 'val_loss']
     for col in required_columns:
         if col not in history.columns:
             raise Exception(f"Column '{col}' not found in history")
 
     loss = history['loss']
     val_loss = history['val_loss']
-    acc = history['accuracy']
-    val_acc = history['val_accuracy']
+    acc = history['acc']
+    val_acc = history['val_acc']
     
     assert len(loss) == len(acc) == len(val_loss) == len(val_acc)
 
     plt.clf()
     epochs = range(len(loss))
-    fig, axs = plt.subplots(2, 1, figsize=(14, 8))
-
+    fig, axs = plt.subplots(2, 1, figsize=(25, 10)) 
     axs[0].plot(epochs, acc, 'bo--', label='Training accuracy')
     axs[0].plot(epochs, val_acc, 'b-', label='Validation accuracy')
     axs[0].set_title('Training and Validation Accuracy')
     axs[0].set_xlabel('Epochs')
     axs[0].set_ylabel('Accuracy')
-    axs[0].legend()
-
+    axs[0].legend(['Train', 'Validation'])
+    
     axs[1].plot(epochs, loss, 'ro--', label='Training loss')
     axs[1].plot(epochs, val_loss, 'r-', label='Validation loss')
     axs[1].set_title('Training and Validation Loss')
     axs[1].set_xlabel('Epochs')
     axs[1].set_ylabel('Loss')
-    axs[1].legend()
-
-    plt.tight_layout()
+    axs[1].legend(['Train', 'Validation'])
     plt.show()
-    
-def plot_images_from_generator(generator, n):
-    for i in range(n):
-        batch = next(generator)
-        images = batch[0] # 0 - images, 1 - labels
-
-        image_augmented = images[0]  # (batch_size, height, width, channels) -> (height, width, channels)
-        plt.imshow(image_augmented)
-        plt.axis('off')
-        plt.show()
         
         
 def plot_grid_from_generator(generator, rows, cols, cmap='viridis'):
@@ -116,12 +106,21 @@ def plot_grid_from_generator(generator, rows, cols, cmap='viridis'):
     axes = axes.flatten()  
 
     for i in range(rows * cols):
-        batch = next(generator)
-        images = batch[0]  
-
-        image_augmented = images[0] 
+        batch = next(generator) 
+        images = batch[0]                               # 0 - images, 1 - labels
+        image_augmented = images[0]                     # (batch_size, height, width, channels) -> (height, width, channels)
         axes[i].imshow(image_augmented, cmap=cmap)
         axes[i].axis('off')  
 
     plt.tight_layout() 
+    plt.show()
+    
+def plot_confusion_matrix(y_true, y_pred, class_names):
+    cm = confusion_matrix(y_true, y_pred)
+
+    plt.figure(figsize=(8, 6))
+    sns.heatmap(cm, annot=True, fmt="d", cmap="Blues", xticklabels=class_names, yticklabels=class_names)
+    plt.xlabel("Predicted Label")
+    plt.ylabel("True Label")
+    plt.title("Confusion Matrix")
     plt.show()
